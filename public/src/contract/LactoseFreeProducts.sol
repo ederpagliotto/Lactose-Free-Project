39d7d3a105d6e8d9e91ce09de0b6f96733c4c9c8
Eder Pagliotto <ederpagliotto@gmail.com>
2024-07-17 19:00:30 +0100

    adding comments on contract

--- public/src/contract/LactoseFreeProducts.sol
+++ public/src/contract/LactoseFreeProducts.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// Struct to represent a product
 contract LactoseFreeProducts {
     struct Product {
         uint256 id;
@@ -10,11 +11,14 @@ contract LactoseFreeProducts {
         string[] documentHashes;
         bool isVerified;
     }
-
+    // Mapping to store products by their ID
     mapping(uint256 => Product) public products;
-    mapping(string => uint256) private nameToId; // Mapping from product name to product ID
+    // Mapping to store product ID by their name
+    mapping(string => uint256) private nameToId;
+    // Counter for the total number of products
     uint256 public productCount;
 
+    // Function to register a new product
     function registerProduct(
         uint256 _id,
         string memory _name,
@@ -22,10 +26,13 @@ contract LactoseFreeProducts {
         string memory _details,
         string[] memory _documentHashes
     ) public {
+        // Ensure the product ID and name are unique
         require(products[_id].id == 0, "Product ID already exists.");
         require(nameToId[_name] == 0, "Product name already exists.");
 
+        //incrementing
         productCount++;
+        // Add new product to the products mapping
         products[_id] = Product(
             _id,
             _name,
@@ -34,15 +41,16 @@ contract LactoseFreeProducts {
             _documentHashes,
             false
         );
-
-        nameToId[_name] = _id; // Store the mapping from name to ID
+        // Store the mapping from name to ID
+        nameToId[_name] = _id;
     }
-
+    // Function to verify a product by its ID
     function verifyProduct(uint256 _productId) public {
         Product storage product = products[_productId];
         product.isVerified = true;
     }
 
+    // Function to get product details by its ID
     function getProductById(
         uint256 _productId
     )
@@ -68,6 +76,7 @@ contract LactoseFreeProducts {
         );
     }
 
+    // Function to get product details by its name
     function getProductByName(
         string memory _name
     )
