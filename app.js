e05bd8866f1e0649ac97b7f796ba763ae3bf4cb4
Eder Pagliotto <ederpagliotto@gmail.com>
2024-08-09 19:13:38 +0100

    updating app file

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const path = require('path');
 const app = express();
 const port = 8080;
 
+app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.set('views', './views');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
